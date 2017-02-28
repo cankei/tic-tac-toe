5c05d095f938d1d7473d1fec94748d0d3c7b89e2
Qi Chen <qi.chen@rea-group.com>
2017-03-01 09:10:45 +1100

    added winning rule checking

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-angular.module('ticTacToeApp', []).controller('TicTacToeController', function (TicTacToeBoardService) {
+angular.module('ticTacToeApp', []).controller('TicTacToeController', function (TicTacToeBoardService, $timeout) {
   var ticTacToe = this;
 
   var PLAYERS = {
@@ -6,51 +6,70 @@ angular.module('ticTacToeApp', []).controller('TicTacToeController', function (T
     1: 'O'
   };
 
-  TicTacToeBoardService.reset();
-  ticTacToe.board = TicTacToeBoardService.board;
-  ticTacToe.placeAt = TicTacToeBoardService.placeAt;
+  ticTacToe.restart = function () {
+    TicTacToeBoardService.reset();
+    ticTacToe.board = TicTacToeBoardService.board();
+  };
+  ticTacToe.restart();
+
+  ticTacToe.placeAt = function (row, column, player) {
+    TicTacToeBoardService.placeAt(row, column, player);
+
+    $timeout(function () {
+      var winner = TicTacToeBoardService.checkWinner();
+      if (winner) alert("Winner is " + winner);
+      else if (TicTacToeBoardService.isFull()) alert("Draw game");
+    }, 100);
+  };
 
   ticTacToe.getCurrentPlayer = function () {
-    return PLAYERS[TicTacToeBoardService.totalPlacedCells % 2];
+    return PLAYERS[TicTacToeBoardService.getTotalPlacedCells() % 2];
   };
 }).factory('TicTacToeBoardService', function () {
   var self = this;
-  var TOTAL_CELLS = 9;
-  self.board = [];
-  self.totalPlacedCells = 0;
+  var SIZE = 3;
+  var board = [];
+  var totalPlacedCells = 0;
 
-  self.reset = function () {
-    self.totalPlacedCells = 0;
-    self.board = [
-      [
-        {value: ''},
-        {value: ''},
-        {value: ''}
-      ],
-      [
-        {value: ''},
-        {value: ''},
-        {value: ''}
-      ],
-      [
-        {value: ''},
-        {value: ''},
-        {value: ''}
+  return {
+    board: function () {
+      return board;
+    },
+    getTotalPlacedCells: function () {
+      return totalPlacedCells;
+    },
+    reset: function () {
+      totalPlacedCells = 0;
+      board = [
+        [
+          {value: ''}, {value: ''}, {value: ''}
+        ], [
+          {value: ''}, {value: ''}, {value: ''}
+        ], [
+          {value: ''}, {value: ''}, {value: ''}
+        ]
       ]
-    ]
-  };
-
-  self.isEnded = function () {
-    return self.totalPlacedCells >= TOTAL_CELLS;
-  };
-
-  self.placeAt = function (row, column, player) {
-    var cell = self.board[row][column];
-    if (!cell.value) {
-      cell.value = player;
-      self.totalPlacedCells++;
+    },
+    placeAt: function (row, column, player) {
+      var cell = board[row][column];
+      if (!cell.value) {
+        cell.value = player;
+        totalPlacedCells++;
+      }
+    },
+    isFull: function () {
+      return totalPlacedCells >= SIZE * SIZE;
+    },
+    checkWinner: function () {
+      for (var i = 0; i < SIZE; ++i) {
+        if (board[i][0].value && board[i][0].value === board[i][1].value && board[i][0].value === board[i][2].value)
+          return board[i][0].value;
+        if (board[0][i].value && board[0][i].value === board[1][i].value && board[0][i].value === board[2][i].value)
+          return board[0][i].value;
+      }
+      if (board[0][0].value && board[0][0].value === board[1][1].value && board[0][0].value === board[2][2].value)
+        return board[0][0].value;
+      if (board[0][2].value === board[1][1].value && board[0][2].value === board[2][0].value) return board[0][2].value;
     }
   };
-
-  return self;
 });
